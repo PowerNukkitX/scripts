ddd0e5695af6b148deb3955bc87098789db17bd7
CoolLoong <1542536763@qq.com>
2024-05-30 21:40:24 +0800

    feat: update scripts

--- start.bat
+++ start.bat
@@ -6,12 +6,11 @@ set JAR_NAME="powernukkitx.jar"
 %JAVA_CMD% -Dfile.encoding=UTF-8 ^
     -Djansi.passthrough=true ^
     -Dterminal.ansi=true ^
-    -XX:+UnlockExperimentalVMOptions ^
-    -XX:+UseG1GC ^
+    -XX:+UseZGC ^
+    -XX:+ZGenerational ^
     -XX:+UseStringDeduplication ^
-    -XX:+EnableJVMCI ^
-    --module-path="./libs/graal-sdk-23.0.0.jar;./libs/truffle-api-23.0.0.jar" ^
     --add-opens java.base/java.lang=ALL-UNNAMED ^
     --add-opens java.base/java.io=ALL-UNNAMED ^
+    --add-opens java.base/java.net=ALL-UNNAMED ^
     -cp %JAR_NAME%;./libs/* ^
     cn.nukkit.Nukkit
