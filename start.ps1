842490e6ae1e96c921b800f6cc3c362287426fda
AzaleeX <azaleemcpe@gmail.com>
2024-08-05 09:28:46 +0200

    feat(start-file): Add verification to all PowerNukkitX start files

--- start.ps1
+++ start.ps1
@@ -1,3 +1,24 @@
+# Check if Java is installed
+if (-not (Get-Command java -ErrorAction SilentlyContinue)) {
+    Write-Host "Java is not installed. Please install Java 21 or higher."
+    Write-Host "You can refer to the installation instructions at https://docs-pnx.pages.dev/requirements"
+    exit 1
+}
+
+# Check if powernukkitx.jar exists
+if (-not (Test-Path -Path "powernukkitx.jar")) {
+    Write-Host "PowerNukkitX.jar not found"
+    Write-Host "You can download the file from https://github.com/PowerNukkitX/PowerNukkitX/releases"
+    exit 1
+}
+
+# Check if libs directory exists
+if (-not (Test-Path -Path "libs" -PathType Container)) {
+    Write-Host "The libs directory was not found. Please ensure the directory is in the current directory."
+    Write-Host "You can download the directory from https://github.com/PowerNukkitX/PowerNukkitX/releases"
+    exit 1
+}
+
 $JAVA_CMD = "java"
 $JAR_NAME = "powernukkitx.jar"
 
--- start.sh
+++ start.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
-# Construct the full path to the Java executable
+echo "PowerNukkitX server software for Minecraft: Bedrock Edition"
+
+# Check if Java is installed
+if ! java -version &> /dev/null
+then
+    echo "Java is not installed. Please install Java 21 or higher."
+    echo "You can refer to the installation instructions at https://docs-pnx.pages.dev/requirements"
+    exit 1
+fi
+
+# Check if powernukkitx.jar exists
+if [ ! -f "powernukkitx.jar" ]; then
+    echo "PowerNukkitX.jar not found"
+    echo "You can download the file from https://github.com/PowerNukkitX/PowerNukkitX/releases"
+    exit 1
+fi
+
+# Check if libs directory exists
+if [ ! -d "libs" ]; then
+    echo "The libs directory was not found. Please ensure the directory is in the current directory."
+    echo "You can download the directory from https://github.com/PowerNukkitX/PowerNukkitX/releases"
+    exit 1
+fi
+
 JAVA_CMD="java"
 JAR_NAME="powernukkitx.jar"
 
-# Execute the Java command
-"$JAVA_CMD" -Dfile.encoding=UTF-8 \
+$JAVA_CMD -Dfile.encoding=UTF-8 \
     -Djansi.passthrough=true \
     -Dterminal.ansi=true \
     -XX:+UseZGC \
@@ -14,5 +36,5 @@ JAR_NAME="powernukkitx.jar"
     --add-opens java.base/java.lang=ALL-UNNAMED \
     --add-opens java.base/java.io=ALL-UNNAMED \
     --add-opens java.base/java.net=ALL-UNNAMED \
-    -cp "$JAR_NAME:./libs/*" \
+    -cp $JAR_NAME:./libs/* \
     cn.nukkit.Nukkit
